55840c282a9563175978abafb1b4fcec0f9418c2
ltnghiammo <138772768+ltnghiammo@users.noreply.github.com>
2025-05-18 23:12:43 -0400

    Update proxy_api.py

---
+++ proxy_api.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+import requests
+import os
+
+app = Flask(__name__)
+
+# Example AbuseIPDB endpoint
+@app.route('/abuseipdb')
+def abuseipdb():
+    ip = request.args.get('ip')
+    API_KEY = os.environ.get("ABUSEIPDB_KEY", "e2997ad9fbbd3446bec838c5d282ce89260314ba9d5176126446fb8ee9703746c4479c7ea50f5ae3")
+    url = f"https://api.abuseipdb.com/api/v2/check?ipAddress={ip}&maxAgeInDays=90"
+    headers = {"Accept": "application/json", "Key": API_KEY}
+    resp = requests.get(url, headers=headers, timeout=5)
+    return jsonify(resp.json())
+
+# Example MaxMind endpoint
+@app.route('/maxmind')
+def maxmind():
+    ip = request.args.get('ip')
+    ACCOUNT_ID = os.environ.get("MAXMIND_ACCOUNT_ID", "1170962")
+    LICENSE_KEY = os.environ.get("MAXMIND_LICENSE_KEY", "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")
+    url = 'https://minfraud.maxmind.com/minfraud/v2.0/score'
+    headers = {'Content-Type': 'application/json'}
+    data = {"device": {"ip_address": ip}}
+    resp = requests.post(url, json=data, auth=(ACCOUNT_ID, LICENSE_KEY), timeout=5)
+    return jsonify(resp.json())
+
+@app.route('/')
+def home():
+    return 'API ready!'
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
